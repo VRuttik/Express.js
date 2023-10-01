7377b263f1615f1e450252e8ce70aa1a28cf6248
Vruttik Moragha <97348689+VRuttik@users.noreply.github.com>
2023-10-01 11:36:19 +0530

    Create index2.js
    Express JS Routing.

---
+++ index2.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = 4000;
+
+app.get("/", (req, res) => {
+    res.send("Welcome to my home page");
+});
+
+app.get("/about", (req, res) => {
+    res.status(200).send("Welcome to my about page");
+});
+
+app.get("/contact", (req, res) => {
+    res.send("Welcome to my contact page");
+});
+
+app.listen(port, () => {
+    console.log(`listening to the port no ${port}`)
+});
